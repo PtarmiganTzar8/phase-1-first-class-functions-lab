4adb1e59da25e1be5405b8655ef0382ec98ca929
Michael <michael.d.lange88@gmail.com>
2022-09-18 17:14:50 -0700

    commit tree 1eff8081ab41c906cc3dff414b583cfc2a0d68d5

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = x => y => x*y
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, input){
+    if (input === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arrayOfDrivers)
+    }
+    else if (input === returnLastTwoDrivers){
+        return returnLastTwoDrivers(arrayOfDrivers)
+    }
+}
+
+// function selectDifferentDrivers(arrayOfDrivers, input){
+//         return input(arrayOfDrivers)
+// }
+
+// function selectDifferentDrivers(arrayOfDrivers, input){}
+//console.log(selectDifferentDrivers(['1', '2', '3', '4', '5', '6', '7', '8'], returnFirstTwoDrivers))
+
+// have function be dynamic
